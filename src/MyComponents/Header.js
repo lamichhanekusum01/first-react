9f019dfede8f22d4444c90d540a545aebb47fb35
Kusum <Kusumlamichhane2015@gmail.com>
2022-03-09 23:05:56 +0545

    Initals

---
+++ src/MyComponents/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Header(props) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">{props.title}</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+      </li>
+     
+    { props.hide ? <li className="nav-item">
+        <a className="nav-link" href="#">About</a>
+      </li>:""
+      }
+    </ul>
+    
+  </div>
+</nav>
+  )
+}
+Header.propTypes ={
+  title: PropTypes.string,
+  Hide:PropTypes.bool.isRequired
+}
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Todos = () => {
+  return (
+<h3>Todos List</h3>  )
+}
